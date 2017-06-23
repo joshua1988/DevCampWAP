69b7d8a0082bfabe4a3a59a9e30fd74e3e71e575
joshua1988 <jangkeehyo@gmail.com>
2017-06-23 16:59:47 +0900

    updated the firebase db code tutorials

---
+++ #5-vuejs-firebase/firebase/samples/#3_firebase-db/js/firebase-db.js
@@ -0,0 +1,45 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+// App Logic
+var input = document.querySelector('input');
+var list = document.querySelector('p');
+var addBtn = document.querySelector('button');
+addBtn.addEventListener('click', addToDB);
+
+// Firebase Monitor the data change
+var dbRef = firebase.database().ref('inputs/');
+dbRef.on('value', function(snapshot) {
+  // console.log(snapshot.val());
+  appendItemsToList(snapshot.val());
+});
+
+// Event Methods
+function addToDB() {
+  firebase.database().ref('inputs/' + getId()).set({
+    input : getInput()
+  });
+  clearInput();
+}
+
+function appendItemsToList(obj) {
+  var arr = [];
+  for (key in obj) {
+    // console.log(obj[key].input);
+    arr.push(obj[key].input);
+  }
+  return list.innerText = arr;
+}
+
+function getInput() {
+  return input.value;
+}
+
+function getId() {
+  var date = new Date();
+  return date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+}
+
+function clearInput() {
+  return input.value = '';
+}
---
+++ #5-vuejs-firebase/firebase/samples/#3_firebase-db/js/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA7y_ic_AHAp1o_3nXZJCTVmUnPqjXl48I",
+  authDomain: "friendlychat-34059.firebaseapp.com",
+  databaseURL: "https://friendlychat-34059.firebaseio.com",
+  projectId: "friendlychat-34059",
+  storageBucket: "friendlychat-34059.appspot.com",
+  messagingSenderId: "800635767370"
+};
+firebase.initializeApp(config);
