12cdd7cac37cc3ba007eb61d9937b7b6bfaa91a2
joshua1988 <jangkeehyo@gmail.com>
2017-06-26 16:09:44 +0900

    Modified the code sample in Components for Global and Local alike

--- #5-vuejs-firebase/vue/samples/#1_components/js/app.js
+++ #5-vuejs-firebase/vue/samples/#1_components/js/app.js
@@ -1,10 +1,20 @@
+// Global Component
 Vue.component('todo-item', {
   template: '<p>This is a child component</p>'
 });
+
+// Local Component
+// var todoItem = {
+//   template: '<p>This is a child component</p>'
+// }
 
 var app = new Vue({
   el: '#app',
   data: {
     message : 'This is a parent component'
-  }
+  },
+  // Local Component
+  // components: {
+  //   'todo-item' : todoItem
+  // }
 });
