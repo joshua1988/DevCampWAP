7865dc8dd5e60ea704c1301845146c74a0b4ced4
joshua1988 <jangkeehyo@gmail.com>
2017-06-26 11:47:07 +0900

    Added the modal to pop up when nth has been typed in todo item

--- #5-vuejs-firebase/vue/samples/#8_vue-todo/src/components/TodoInput.vue
+++ #5-vuejs-firebase/vue/samples/#8_vue-todo/src/components/TodoInput.vue
@@ -1,25 +1,46 @@
 <template lang="html">
   <div class="inputBox">
-    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
     <span class="addContainer" v-on:click="addTodo"><i class="addBtn fa fa-plus" aria-hidden="true"></i></span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <!--
+        you can use custom content here to overwrite
+        default content
+      -->
+      <h3 slot="header">Warning</h3>
+      <span slot="footer" @click="showModal = false">Don't enter the empty item
+        <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   props: ['passedData'],
   data () {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
     addTodo() {
-      var value = this.newTodoItem && this.newTodoItem.trim();
-      localStorage.setItem(value, value);
-      this.passedData.push(value);
-      this.newTodoItem = '';
+      if (this.newTodoItem !== '') {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.passedData.push(value);
+        this.newTodoItem = '';
+      } else {
+        this.showModal = true;
+      }
     }
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
