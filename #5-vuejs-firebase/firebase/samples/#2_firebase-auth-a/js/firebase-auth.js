eba1498c305d80c4b87befa9f505dbae0669b905
joshua1988 <jangkeehyo@gmail.com>
2017-06-29 14:26:58 +0900

    Updated the firebase samples to make the steps easier for students

---
+++ #5-vuejs-firebase/firebase/samples/#2_firebase-auth-a/js/firebase-auth.js
@@ -0,0 +1,47 @@
+var input = document.querySelectorAll('input');
+var button = document.querySelectorAll('button');
+var log = document.querySelector('p');
+
+var firebaseSignIn = function () {
+
+};
+
+var firebaseSignInWithGoogle = function () {
+
+};
+
+var firebaseSignUp = function () {
+
+};
+
+var firebaseSignOut = function () {
+  // 할일 : Sign Out API 찾아서 기능 구현
+
+};
+
+var getEmail = function () {
+  return input[0].value;
+};
+
+var getPassword = function () {
+  return input[1].value;
+};
+
+var exceptionHandler = function () {
+  if ( getEmail() === "" || getPassword() === "") {
+    alert("enter the email and password");
+    return true;
+  }
+  return false;
+};
+
+var clearForm = function () {
+  input[0].value = "";
+  input[1].value = "";
+};
+
+window.onload = function () {
+  button[0].addEventListener('click', firebaseSignIn);
+  button[1].addEventListener('click', firebaseSignInWithGoogle);
+  button[2].addEventListener('click', firebaseSignUp);
+};
---
+++ #5-vuejs-firebase/firebase/samples/#3_firebase-db-a/js/firebase-db.js
@@ -0,0 +1,30 @@
+// App Logic
+var input = document.querySelector('input');
+var list = document.querySelector('p');
+var addBtn = document.querySelector('button');
+addBtn.addEventListener('click', addToDB);
+
+// Firebase Monitor the data change
+
+// Event Methods
+function addToDB() {
+
+}
+
+function appendItemsToList(obj) {
+
+}
+
+// input operation
+function getInput() {
+  return input.value;
+}
+
+function getId() {
+  var date = new Date();
+  return date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+}
+
+function clearInput() {
+  return input.value = '';
+}
